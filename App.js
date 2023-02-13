e4dc71f5e5f5a30ea11e542a39dc09b0dade2d24
KSoft-Solution <kanhasahu955902@gmail.com>
2023-02-13 20:02:54 +0530

    RN  13-02

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { CusImage,CusView } from './src/corecomponents'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CusImage/>
+      <CusView/>
     </View>
   );
 }
---
+++ src/corecomponents/index.js
@@ -0,0 +1,5 @@
+export { default as CusImage } from './Image'
+export { default as CusScroolView } from './ScroolView'
+export { default as CusText } from './Text'
+export { default as CusTextInp } from './TextInput'
+export { default as CusView } from './View'
